9cc225175df6a31a428a656814d46b857f92f02b
Marcos Silva <marcosilva968@gmail.com>
2021-02-09 20:48:09 -0300

    Condicionais if else elif

---
+++ Condicionais_if_else_elif.py
@@ -0,0 +1,13 @@
+"""
+Estruturas condicionais
+if(Se), else(Senão), elif
+"""
+
+idade = 26
+
+if idade < 18:
+    print("Meno de Idade")
+elif idade == 18:
+    print("Tem 18 anos")
+else:
+    print("Maior de Idade")
