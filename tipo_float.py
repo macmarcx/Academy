130a3aebbff88d1e05c7d6ec56f4e4c53ee8298c
Marcos Silva <marcosilva968@gmail.com>
2021-02-09 19:52:44 -0300

    Tipo float

---
+++ tipo_float.py
@@ -0,0 +1,26 @@
+"""
+Tipo Float
+
+Tipo real, decimal
+
+Casas decimais
+
+OBS: O separador de casas decimais na programação é o ponto e não a vírgula.
+"""
+
+valor = 1.44
+print(valor)
+
+# Podemos converter um float para um int
+"""
+OBS: Ao converter valores float para inteiros, nós perdemos precisão.
+"""
+res = int(valor)
+print(res)
+print(type(res))
+
+# Podemos trabalhar com números complexos
+
+variavel = 5j
+print(variavel)
+print(type(variavel))
