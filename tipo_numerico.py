281041deabaef14ee82508c5129103555a5413ff
Marcos Silva <marcosilva968@gmail.com>
2021-02-09 16:47:54 -0300

    Tipo Numerico

---
+++ tipo_numerico.py
@@ -0,0 +1,30 @@
+"""
+Tipo Numérico
+"""
+# Soma
+soma = 2 + 2
+print(f"A soma é: {soma}")
+
+# Subtração
+subtracao = 3 - 2
+print(f"A subtração é: {subtracao}")
+
+# Multiplicação
+multiplica = 2 * 3
+print(f"A multiplicação é: {multiplica}")
+
+# Divisão com ponto flutuante
+dividir = 6 / 2
+print(f"A divisão é: {dividir}")
+
+# Divisão com numero real
+dividir = 6 // 2
+print(f"A divisão é: {dividir}")
+
+# Fatoração
+fatorar = 5 % 2
+print(f"A fatoração é: {fatorar}")
+
+# Valor elevado ao quadrado
+elevado = 2 ** 2
+print(f"O número elevado ao quaddrado é: {elevado}")
