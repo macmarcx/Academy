c08eb1c7df6aabe7ccfcaec14b4bc6d58580ed33
Marcos Silva <marcosilva968@gmail.com>
2021-02-10 19:22:54 -0300

    Tuplas

---
+++ Tuplas.py
@@ -0,0 +1,158 @@
+"""
+Tuplas (tuple)
+
+Tuplas são bastante parecidas com listas.
+
+Existem basicamente duas diferenças básicas:
+
+1 - As tuplas são representadas por parênteses ()
+
+2 - As tuplas são imutáveis. Isso significa que ao se criar uma tupla ela não muda. Toda operação em uma tupla
+gera uma nova tupla.
+
+
+
+# CUIDADO 1: As tuplas são representadas por (), mas veja:
+tupla1 = (1, 2, 3, 4, 5, 6)
+print(tupla1)
+
+print(type(tupla1))
+
+tupla2 = 1, 2, 3, 4, 5, 6
+print(tupla2)
+
+print(type(tupla2))
+
+# Cuidado 2: Tuplas com 1 elemento
+
+tupla3 = (4) # Isso não é uma tupla!
+print(tupla3)
+
+print(type(tupla3))
+
+tupla4 = (4, ) # Isso é uma tupla!
+print(tupla4)
+
+print(type(tupla4))
+
+tupla5 = 4,
+print(tupla5)
+
+print(type(tupla5))
+
+# CONCLUSÃO: Podemos concluir que tuplas são definidas pela virgula e não pelo uso do parênteses
+
+# Podemos gerar uma tupla dinamicamente com range(inicio, fim, passo)
+tupla = tuple(range(11))
+
+print(tupla)
+print(type(tupla))
+
+# Desempacotamento de tupla
+
+tupla = ('Marcos Silva', 'Programação em Python: Essencial')
+
+escola, curso = tupla
+
+print(escola)
+print(curso)
+
+# OBS: Gera erro (ValueError) se colocarmos um número diferente de elementos para desenpacotar.
+
+# Métodos para adição e remoção de elementos nas tuplas não existem, dado o fato das tuplas serem imutáveis.
+
+# Soma, Valor Máximo, Valor Mínimo e Tamanho
+
+# Se os valores forem todos inteiros ou reais
+
+tupla = (1, 2, 3, 4, 5, 6)
+
+print(sum(tupla))
+print(max(tupla))
+print(min(tupla))
+print(len(tupla))
+
+# Concatenação de tuplas
+
+tupla1 = (1, 2, 3)
+print(tupla1)
+
+tupla2 = (4, 5, 6)
+print(tupla2)
+
+print(tupla1 + tupla2) # Tuplas são imutáveis
+
+print(tupla1)
+print(tupla2)
+
+tupla3 = tupla1 + tupla2
+
+print(tupla3)
+print(tupla1)
+print(tupla2)
+
+tupla1 = tupla1 + tupla2 # Tuplas são imutáveis, mas podemos sobrescrever seus valores
+
+print(tupla1)
+
+# Verificar se determinado elemento está contido na tupla
+
+tupla = (1, 2, 3)
+
+print(3 in tupla)
+
+# Iterando sobre uma tupla
+
+tupla = (1, 2, 3)
+
+for n in tupla:
+    print(n)
+
+for indice, valor in enumerate(tupla):
+    print(indice, valor)
+
+# Contando elementos dentro de uma tupla
+
+tupla = ('a', 'b', 'c', 'd', 'e', 'a', 'b')
+
+print(tupla.count('c'))
+
+escola = tuple('Marcos Silva')
+print(escola)
+
+print(escola.count('a'))
+
+# Dicas na utilização de tuplas
+
+# Devemos utilizar tuplas SEMPRE que não precisarmos modificar os dados contidos em uma coleção
+
+# Exemplo 1
+
+meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+
+print(meses)
+
+# O acesso a elementos de uma tupla também é semelhante a de uma lista
+
+print(meses[5])
+
+# Iterar com while
+
+i = 0
+
+while i < len(meses):
+    print(meses[i])
+    i += 1
+"""
+
+# Dicas na utilização de tuplas
+
+# Devemos utilizar tuplas SEMPRE que não precisarmos modificar os dados contidos em uma coleção
+
+# Exemplo 1
+
+meses = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
+
+# Verificamos em qual indice um elemento está na tupla
+
+print(meses.index('Junho'))
