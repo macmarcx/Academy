0d0d418598a9e902e6b903b742b09cbbb797a981
Marcos Silva <marcosilva968@gmail.com>
2021-02-10 21:00:50 -0300

    Mapas

---
+++ mapas.py
@@ -0,0 +1,51 @@
+"""
+Mapas -> Conhecidos em Python como Dicionários
+
+Dicionários em Python são representados por chaves {}
+
+receita = {'jan': 100, 'fev': 250, 'mar': 400}
+
+# Iterar sobre dicionários
+
+for chave in receita:
+    print(chave)
+
+for chave in receita:
+    print(receita[chave])
+
+for chave in receita:
+    print(f'Em {chave} recebi R$ {receita[chave]}')
+
+# Acessando as chaves
+
+print(receita.keys())
+
+for chave in receita.keys():
+    print(receita[chave])
+
+# Acessando os valores
+
+print(receita.values())
+
+for valor in receita.values():
+    print(valor)
+
+# Desempacotamento de dicionários
+
+print(receita)
+
+for chave, valor in receita.items():
+    print(f'chave = {chave} e valor = {valor}')
+
+"""
+
+receita = {'jan': 100, 'fev': 250, 'mar': 400}
+
+# Soma, Valor Máximo, Valor Mínimo, Tamanho
+
+# Se os valores forem todos inteiros ou reais
+
+print(sum(receita.values()))
+print(max(receita.values()))
+print(min(receita.values()))
+print(len(receita.values()))
