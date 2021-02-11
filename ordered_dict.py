84ff87dd1670cac6262e7b63381b03c5018800d4
Marcos Silva <marcosilva968@gmail.com>
2021-02-11 15:17:03 -0300

    ordered dict

---
+++ ordered_dict.py
@@ -0,0 +1,21 @@
+"""
+Módulo Collections: Ordered Dict
+
+
+
+# Em um dicionário, a ordem de inserção dos elementos não é garantida.
+dicionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+for chave, valor in dicionario.items():
+    print(f'chave={chave}:valor={valor}')
+print(dicionario)
+
+"""
+# Fazendo o import
+
+from collections import OrderedDict
+
+dicionario = OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
+
+for chave, valor in dicionario.items():
+    print(f'chave={chave}:valor={valor}')
