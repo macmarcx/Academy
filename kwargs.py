ab1df00cce0c424d6cd98b5004ba2928df9d6b88
Marcos Silva <marcosilva968@gmail.com>
2021-02-12 20:33:36 -0300

    kwargs

---
+++ kwargs.py
@@ -0,0 +1,67 @@
+"""
+**Kwargs
+
+Poderiamos chamar este parâmetro de **xis, mas por convenção chamamos de **kwargs
+
+Este é só mais um parâmetro, mas diferente do *args que coloca os valores extras
+em uma tupla, o **kwargs exige que utilizemos parâmetros nomeados, e transforma esses
+parâmetros extras em um dicionário.
+
+
+# Exemplo
+
+def cores_favoritas(**kwargs):
+    for chave, valor in kwargs.items():
+        print(f'A cor favorita de {chave.title()} é `{valor}')
+
+cores_favoritas(marcos='verde', julia='amarelo', fernanda='azul', vanessa='branco')
+
+# OBS: Os parâmetros *args e **kwargs não são obrigatórios.
+
+# Exemplo mais complexo
+
+def cumprimento_especial(**kwargs):
+    if 'geek' in kwargs and kwargs['geek'] == 'Python':
+        return 'Você recebeu um cumprimento Pythonico Geek!'
+    elif 'geek' in kwargs:
+        return f"{kwargs['geek']} Geek!"
+    return 'Não tenho certeza quem você é...'
+
+print(cumprimento_especial())
+print(cumprimento_especial(geek='Python'))
+print(cumprimento_especial(geek='Oi'))
+print(cumprimento_especial(geek='especial'))
+
+# Nas nossas funções, podemos ter (Nesta Ordem):
+
+- Parâmetros obrigatórios;
+- *args;
+- Parâmetros default(não obrigatórios);
+- **kwargs.
+"""
+
+# Desempacotar com **kwargs
+
+def mostra_nomes(**kwargs):
+    return f"{kwargs['nome']} {kwargs['sobrenome']}"
+
+nomes = {'nome': 'Felicity', 'sobrenome': 'Jones'}
+
+print(mostra_nomes(**nomes))
+
+def soma_multiplos_numeros(a, b, c):
+    print(a + b + c)
+
+lista = [1, 2, 3]
+tupla = (1, 2, 3)
+conjunto = {1, 2, 3}
+
+soma_multiplos_numeros(*lista)
+soma_multiplos_numeros(*tupla)
+soma_multiplos_numeros(*conjunto)
+
+dicionario = dict(a=1, b=2, c=3)
+
+soma_multiplos_numeros(**dicionario)
+
+# OBS: Os nomes da chave em um dicionario devem ser o mesmo dos parâmetros da função
